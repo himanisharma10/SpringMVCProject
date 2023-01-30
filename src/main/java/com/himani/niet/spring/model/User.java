a9a77d7359274f28e8ccc6d817339018943f3bf1
Himani Sharma <him.win@gmail.com>
2023-01-31 01:19:07 +0530

    first commit

---
+++ src/main/java/com/himani/niet/spring/model/User.java
@@ -0,0 +1,18 @@
+package com.himani.niet.spring.model;
+
+/**
+ * 
+ * @author Himani Sharma
+ *
+ */
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
